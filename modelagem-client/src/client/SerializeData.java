8bf0905fb3425498565aa114551500e98b7099b4
Rifeli <ritorfl@gmail.com>
2016-06-16 03:46:52 -0300

    Seriealized and tested

--- modelagem-client/src/client/SerializeData.java
+++ modelagem-client/src/client/SerializeData.java
@@ -12,13 +12,41 @@ public class SerializeData {
 	
 	public void sendData(String type, String v1){
 		String seriealizedString = null;
+		System.out.println("Distribuindo");
 		if(type.compareTo("selectPlayer") == 0){
 			String name = v1;
 			Client.setMessage(seriealizedString);
 		}
+		else if (type.compareTo("Distribuir") == 0){
+			String cor = v1;
+			seriealizedString = "Distribuir:" + cor;
+			Client.setMessage(seriealizedString);
+		}
 	}
-	public void sendData(String type, String v1, String v2){
-			
+	public void sendData(String type, String v1, String v2, String v3, String v4, String v5, String v6){
+		String seriealizedString = null;
+		if(type.compareTo("Atacar") == 0){
+			System.out.println("Atacando");
+			String name1 = v1;
+			String cor1 = v2;
+			String numeroSoldados1 = v3;
+			String name2 = v4;
+			String cor2 = v5;
+			String numeroSoldados2 = v6;
+			seriealizedString = "Atacar:" + name1 + "," + cor1 + "," + numeroSoldados1 + "," + name2 + "," + cor2 + "," + numeroSoldados2;
+			Client.setMessage(seriealizedString);
+		}
+		else if(type.compareTo("Remanejar") == 0){
+			System.out.println("Remanejando");
+			String name1 = v1;
+			String cor1 = v2;
+			String numeroSoldados1 = v3;
+			String name2 = v4;
+			String cor2 = v5;
+			String numeroSoldados2 = v6;
+			seriealizedString = "Remanejar:" + name1 + "," + cor1 + "," + numeroSoldados1 + "," + name2 + "," + cor2 + "," + numeroSoldados2;
+			Client.setMessage(seriealizedString);
+		}
 	}
 	public void sendData(String type, String v1, String v2, String v3){
 		
